821f8a5e2f316116f58f6a715fdcbc4ad480a158
aisarthak <84112158+cooldevloper143@users.noreply.github.com>
2023-11-11 21:41:03 +0530

    fail to complete on my own

---
+++ bgcolorchanger/001/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [backgroundColor, setBackgroundColor] = useState("white");
+
+  const changeBackground = (color) => {
+    setBackgroundColor(color);
+  };
+
+  return (
+    <div
+      style={{ backgroundColor, height: "100vh", transition: "background-color 0.5s" }}
+    >
+      <button onClick={() => changeBackground("#ffcc00")}>
+        Change to Yellow
+      </button>
+      <button onClick={() => changeBackground("#00ffcc")}>
+        Change to Green
+      </button>
+      <button onClick={() => changeBackground("#cc00ff")}>
+        Change to Purple
+      </button>
+    </div>
+  );
+}
