d6deed72c517b8643f0233ee0c7ad4e945c198d5
aisarthak <84112158+cooldevloper143@users.noreply.github.com>
2023-11-11 15:14:48 +0530

    props.username successful cuatome name for each card

--- 03tailwindprops/src/App.js
+++ 03tailwindprops/src/App.js
@@ -2,12 +2,17 @@
 import "./App.css";
 import Card from "./components/card";
 function App() {
+  let myObj = {
+    username : "sarthka",
+    age : 17,
+  }
+  let myArr = [ 1,2,3,4]
   return (
     <>
      <Card/>
-     <Card channel="chai aur code"/>
-     <Card />
-     <Card/>
+     <Card username="chai aur code" someObj={myObj} someArr={myArr}/>
+     <Card username="chai sutta bar"/>
+     <Card username="btech chai wali"/> 
      
 
     </>
