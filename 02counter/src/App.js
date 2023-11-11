cb8fd1fedd0e64d8db0f48a1a92f3ed651bdf0c9
aisarthak <84112158+cooldevloper143@users.noreply.github.com>
2023-11-11 12:48:16 +0530

    learnd how to use usestate and how to make changes in ui

--- 02counter/src/App.js
+++ 02counter/src/App.js
@@ -1,28 +1,30 @@
+import { useState} from "react";
+
 function App() {
+  var [counter, setCounter] = useState(15);
+  // let counter = 1;
 
-  let counter = 1;
-
-  const addValue = () => {
+  var addValue = () => {
     // console.log("value added",Math.random());
-    counter = counter + 1
-    console.log(counter)
-    return counter;
-  }
+    // setCounter = counter + 1
+    // console.log(setCounter)
+    // return counter;
+    setCounter(counter + 1);
+  };
+  var removevalue = () => {
+    setCounter(counter - 1);
+  };
 
   return (
     <>
       <h1>chai aur react</h1>
       <h2>counter value : {counter}</h2>
 
-      <button
-      onClick={addValue}
-      >Add value</button>
+      <button onClick={addValue}>Add value</button>
       <br />
-      <button>reamove value</button>
+      <button onClick={removevalue}>reamove value</button>
     </>
   );
 }
 
 export default App;
-
-
