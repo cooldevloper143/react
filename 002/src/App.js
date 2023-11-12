dd8d5dc661a45b79baccc2bff9cb429be2bbd9fa
aisarthak <84112158+cooldevloper143@users.noreply.github.com>
2023-11-12 13:42:35 +0530

    completed learn how to use correct variable names and proper function and loop really you need to master js before react

--- 002/src/App.js
+++ 002/src/App.js
@@ -1,10 +1,74 @@
-// import logo from './logo.svg';
-import './App.css';
-// eslint-disable-next-line
-export default function () {
-  return (
-   <div className='container'>
+/* eslint-disable */
+import React, { useState, useCallback, useEffect } from "react";
+import "./App.css";
 
-   </div>
+export default function PasswordGenerator() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharacterAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (numberAllowed) {
+      str += "0123456789";
+    }
+    if (characterAllowed) {
+      str += "!@#$%^&*()_+-=[]{}|;:,.<>?/";
+    }
+
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, characterAllowed]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, characterAllowed, passwordGenerator]);
+  return (
+    <>
+      <div className="main">
+        <h2>Password Generator</h2>
+        <div className="pass">
+          <input
+            type="text"
+            value={password}
+            placeholder="password"
+            className="inputtext"
+            readOnly
+          />
+          <button className="copy">copy</button>
+        </div>
+        <div className="input">
+          <input
+            type="range"
+            className="range"
+            min={8}
+            max={20}
+            value={length}
+            onChange={(e) => setLength(e.target.value)}
+          />
+          <label className="labellength">Length={length}</label>
+
+          <input
+            type="checkbox"
+            className="nobox"
+            onChange={() => setNumberAllowed(!numberAllowed)}
+          />
+          <label className="labellength">Number</label>
+
+          <input
+            type="checkbox"
+            className="charbox"
+            onChange={() => setCharacterAllowed(!characterAllowed)}
+          />
+          <label className="labellength">Characters</label>
+        </div>
+      </div>
+    </>
   );
-};
+}
