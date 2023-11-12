edb3ad1da312d893d678ff5479384cdc19f110ef
aisarthak <84112158+cooldevloper143@users.noreply.github.com>
2023-11-12 17:15:02 +0530

    thoda ho gya

--- 003/src/App.js
+++ 003/src/App.js
@@ -1,11 +1,45 @@
 // import logo from './logo.svg';
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <>
-    
+      <div className="fullpage">
+        <div className="container">
+          <div className="from">
+            <div className="head">
+              <div>
+                <div>
+                  <h>From</h>
+                </div>
+                <h>Currency Type</h>
+                <div></div>
+              </div>
+            </div>
+            <div className="frominput"></div>
+            <div className="fromct"></div>
+          </div>
+          <div className="swap2">
+            <div className="swap">swap</div>
+          </div>
+          <div className="to">
+            <div className="head">
+              <div>
+                <div id="from">
+                  <h>From</h>
+                </div>
+                <div id="fromtype">
+                  <h>Currency Type</h>
+                </div>
+              </div>
+            </div>
+            <div className="frominput"></div>
+            <div className="fromct"></div>
+          </div>
+          <div className="button"></div>
+        </div>
+      </div>
     </>
   );
 }
