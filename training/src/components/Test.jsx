10085a814859dbea78341de358800ab3504204eb
aisarthak <84112158+cooldevloper143@users.noreply.github.com>
2023-11-17 11:55:38 +0530

    000

---
+++ training/src/components/Test.jsx
@@ -0,0 +1,10 @@
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
+import React from "react";
+
+export default function Test() {
+  return <div></div>;
+}
+
+
+// keyboard shortcut shift + tab  
+// 
