c4d230946fa7057256bbb8a9289ee30f3c482e48
aisarthak <84112158+cooldevloper143@users.noreply.github.com>
2023-11-23 22:27:01 +0530

    000

--- inotebook/inotebook/backend2/routes/auth.js
+++ inotebook/inotebook/backend2/routes/auth.js
@@ -4,13 +4,13 @@ const router = express.Router();
 
 
 // create a user useing POST "/api/auth/" . doesnt required auth 
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
 
     const user = User(req.body);
     user.save()
 
     res.send(req.body)
-    
+
 })
 // export default router;
 module.exports = router;
